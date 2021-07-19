4e5870de530ffe41ed1c99c7f15f65789f0a7c5f
Andrew Brown <44160842+Andrew-Brown1@users.noreply.github.com>
2021-07-19 11:38:05 +0100

    Create download_visual_features.sh
    code to download the visual features

---
+++ scripts/download_visual_features.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A script to download and unpack the visual data for the Condensed Movies Challenge
+
+Output_Directory=""
+
+
+for FILE_INDEX in {'a','b','c','d','e','f','g','h','i','j','k','l'}
+    do
+        wget -c http://pedro.eng.ox.ac.uk/Condensed_Movies_Challenge/features.tar.gz.parta$FILE_INDEX -O - | tar -xz -C $Output_Directory
+    done
